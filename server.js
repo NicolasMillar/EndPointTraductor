737cc537ae36a5d66e04b2f267b6f8fa7f3c9ac2
Nicolas <nicolas.millar.millar@gmail.com>
2023-11-08 18:59:56 -0300

    endpoint para buscar la traduccion agregado

--- server.js
+++ server.js
@@ -15,16 +15,30 @@ app.use(express.json());
     //Endpoint que retorna todos los idiomas a los que se puede traducir
     app.get('/idiomas', async (req, res) => {
         try {
-        const idiomas = await db.any('SELECT * FROM idiomas');
-        res.json(idiomas);
+            const idiomas = await db.any('SELECT * FROM idiomas');
+            res.json(idiomas);
         } catch (error) {
-        console.error('Error al obtener los datos de la tabla "idiomas":', error);
-        res.status(500).json({ error: 'Ocurrió un error al obtener los datos de la tabla "idiomas"' });
+            console.error('Error al obtener los datos de la tabla "idiomas":', error);
+            res.status(500).json({ error: 'Ocurrió un error al obtener los datos de la tabla "idiomas"' });
         }
     });
     
     //Endpoint que busca la traduccion segun el idioma y la palabra de origen
-    app.get('/buscar', (req, res) => {
+    app.get('/buscar', async(req, res) => {
+        const { id_idioma, origen } = req.query;
+        try {
+            if (id_idioma && origen) {
+                const query = `
+                    SELECT * FROM traduccion
+                    WHERE id_idioma = $1 AND origen = $2`;
+                
+                const idiomas = await db.any(query, [id_idioma, origen]);
+                res.json(idiomas);
+            } 
+        } catch (error) {
+            console.error('Error al obtener los datos de la tabla "traduccion":', error);
+            res.status(500).json({ error: 'Ocurrió un error al obtener los datos de la tabla "traduccion"' });
+        }
     });
 
 app.listen(port, () => {
